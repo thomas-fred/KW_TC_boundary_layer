ee3e51c8f00c786cc22f025893ec1612bb2df738
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-02-27 14:46:04 +0000

    Remove old binary, prevents test run with failed compilation

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@
 # ifort -O3 -cpp -qopenmp -ftz -zero -o footprint_model footprint_model.f
 
 # but not on pwlldu, so use GNU fortran
+rm footprint_model
 gfortran -O3 -cpp -std='gnu' -fopenmp -o footprint_model src/main.f
