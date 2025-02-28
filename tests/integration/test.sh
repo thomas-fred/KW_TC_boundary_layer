4275f8961dc264fd027f687cbbfb99d4899a9a23
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2025-02-28 00:10:11 +0000

    Time test duration

--- tests/integration/test.sh
+++ tests/integration/test.sh
@@ -4,25 +4,27 @@
 # To be run from directory containing `tests/`, e.g.
 # ./tests/integration/test.sh maria_PRI_short
 
+TEST_START_TIME=$EPOCHREALTIME
+
 TEST_DIR="tests/integration/$1"
 if [ ! -d ${TEST_DIR} ]; then
     echo "${TEST_DIR} does not exist, quitting"
     exit 1
 fi
 
+STORM_YEAR="MARIA_2017"
+rm -r ${STORM_YEAR}
+mkdir ${STORM_YEAR}
+
 echo "Compiling program"
+./compile.sh
+
+echo "Using following problem configuration for ${STORM_YEAR}"
 TEMP_NAMELIST_FILENAME=$(mktemp)
 cat ./namelist > ${TEMP_NAMELIST_FILENAME}
 rm ./namelist
 cp "${TEST_DIR}/namelist" "./namelist"
-./compile.sh
-
-echo "Using following problem configuration"
 cat ./namelist
-
-STORM_YEAR="MARIA_2017"
-rm -r ${STORM_YEAR}
-mkdir ${STORM_YEAR}
 
 echo "Running boundary layer simulation"
 ./footprint_model
@@ -42,13 +44,33 @@ micromamba run --name kw-pbl ncl ./scripts/postprocess_historical.ncl
 echo "Checking output against reference"
 STORM_PROFILE="maria_WILLOUBY"
 
+# keep track of whether we're in good shape or not
+TEST_STATE=0
+
 diff ${STORM_YEAR}/${STORM_PROFILE}.txt ${TEST_DIR}/${STORM_PROFILE}.txt &>/dev/null
-test $? -eq 0 || echo "Failure: mismatch in computed maximum speeds"
+if [ $? != 0 ]; then
+    echo "Failure: mismatch in computed maximum speeds"
+    TEST_STATE=1
+fi
 
 ./tests/visual_compare.sh ${STORM_YEAR}/${STORM_PROFILE}.pdf ${TEST_DIR}/${STORM_PROFILE}.pdf
-test $? -eq 0 || echo "Failure: mismatch in generated plot"
+if [ $? != 0 ]; then
+    echo "Failure: mismatch in generated plot"
+    TEST_STATE=1
+fi
 
 # replace original namelist
 rm ./namelist
 cp ${TEMP_NAMELIST_FILENAME} ./namelist
 rm ${TEMP_NAMELIST_FILENAME}
+
+ELAPSED_TIME=$(bc -l <<< "$EPOCHREALTIME - $TEST_START_TIME")
+echo "Test took ${ELAPSED_TIME} seconds"
+
+if [ $TEST_STATE != 0 ]; then
+    echo "Test failed :("
+    exit $TEST_STATE
+else
+    echo "Test passed :)"
+    exit $TEST_STATE
+fi
